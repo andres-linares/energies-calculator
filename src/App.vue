33ab1b11fe0e784a0ebd627e16acfe088828b011
andres-linares <andres.linares.971122@gmail.com>
2020-02-19 18:59:37 -0500

    home done

--- src/App.vue
+++ src/App.vue
@@ -2,39 +2,42 @@
   <div id="app">
     <navigation-bar></navigation-bar>
     <div class="d-flex mt-4 justify-content-center flex-wrap">
-      <b-card title="Paneles" img-src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Photovoltaik_Dachanlage_Hannover_-_Schwarze_Heide_-_1_MW.jpg/1200px-Photovoltaik_Dachanlage_Hannover_-_Schwarze_Heide_-_1_MW.jpg" class="m-2" style="width: 20rem">
-        <b-card-text class="text-left">
-          Calcula el area de un panel de energía.
-        </b-card-text>
-      </b-card>
-
-      <b-card title="Demandas" img-src="https://alternativeenergy24.com/wp-content/uploads/2019/09/enery-1.jpg" class="m-2" style="width: 20rem">
-        <b-card-text class="text-left">
-          Calcula la demanda electrica de una casa.
-        </b-card-text>
-      </b-card>
-
-      <b-card title="Cables" img-src="https://upload.wikimedia.org/wikipedia/commons/c/ca/500kV_3-Phase_Transmission_Lines.png" class="m-2" style="width: 20rem">
-        <b-card-text class="text-left">
-          Calcula el area de la sección transversal de un cable.
-        </b-card-text>
-      </b-card>
+      <home-tile
+        title="Paneles"
+        imgUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Photovoltaik_Dachanlage_Hannover_-_Schwarze_Heide_-_1_MW.jpg/1200px-Photovoltaik_Dachanlage_Hannover_-_Schwarze_Heide_-_1_MW.jpg"
+        route
+        body="Calcula el area de un panel de energía."
+      />
+      <home-tile
+        title="Demandas"
+        imgUrl="https://alternativeenergy24.com/wp-content/uploads/2019/09/enery-1.jpg"
+        route
+        body="Calcula la demanda eléctrica de una casa."
+      />
+      <home-tile
+        title="Cables"
+        imgUrl="https://upload.wikimedia.org/wikipedia/commons/c/ca/500kV_3-Phase_Transmission_Lines.png"
+        route
+        body="Calcula el área de la sección transversal de un cable."
+      />
     </div>
   </div>
 </template>
 
 <script>
-import NavigationBar from './components/NavigationBar.vue'
+import NavigationBar from "./components/NavigationBar.vue";
+import HomeTile from "./components/HomeTile.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    NavigationBar
+    NavigationBar,
+    HomeTile
   }
-}
+};
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/HomeTile.vue
@@ -0,0 +1,32 @@
+<template>
+  <b-card
+    :title="title"
+    :img-src="imgUrl"
+    class="m-4 home-card neumorphic"
+    style="width: 20rem"
+  >
+    <b-card-text class="text-left">{{ body }}</b-card-text>
+  </b-card>
+</template>
+
+<script>
+export default {
+    props: [
+        'title',
+        'imgUrl',
+        'route',
+        'body'
+    ]
+};
+</script>
+
+<style scoped lang="scss">
+.home-card {
+    border-radius: .6rem !important;
+    border: none !important;
+
+    .card-img {
+        border-radius: .6rem .6rem 0 0;
+    }
+}
+</style>
