735da7b20d7d49baa0230705a7c55a59ae34e0ea
andres-linares <andres.linares.971122@gmail.com>
2020-02-19 15:54:40 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <navigation-bar></navigation-bar>
+    <div class="d-flex mt-4 justify-content-center flex-wrap">
+      <b-card title="Paneles" img-src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Photovoltaik_Dachanlage_Hannover_-_Schwarze_Heide_-_1_MW.jpg/1200px-Photovoltaik_Dachanlage_Hannover_-_Schwarze_Heide_-_1_MW.jpg" class="m-2" style="width: 20rem">
+        <b-card-text class="text-left">
+          Calcula el area de un panel de energía.
+        </b-card-text>
+      </b-card>
+
+      <b-card title="Demandas" img-src="https://alternativeenergy24.com/wp-content/uploads/2019/09/enery-1.jpg" class="m-2" style="width: 20rem">
+        <b-card-text class="text-left">
+          Calcula la demanda electrica de una casa.
+        </b-card-text>
+      </b-card>
+
+      <b-card title="Cables" img-src="https://upload.wikimedia.org/wikipedia/commons/c/ca/500kV_3-Phase_Transmission_Lines.png" class="m-2" style="width: 20rem">
+        <b-card-text class="text-left">
+          Calcula el area de la sección transversal de un cable.
+        </b-card-text>
+      </b-card>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavigationBar from './components/NavigationBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavigationBar
   }
 }
 </script>
@@ -23,6 +41,10 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.card-img {
+  height: 15rem;
+  object-fit: cover;
 }
 </style>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <b-navbar toggleable="md" type="dark" variant="primary">
+    <b-navbar-brand href="#">Calculador Energías</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="#">Paneles</b-nav-item>
+        <b-nav-item href="#">Demandas</b-nav-item>
+        <b-nav-item href="#">Cables</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
